bc074057652919b89748c9726708c684079a1d60
hoppjan <83405332+hoppjan@users.noreply.github.com>
2026-05-14 19:30:59 +0200

    Use settings in MealView

--- Mensa SH/MealView.swift
+++ Mensa SH/MealView.swift
@@ -3,12 +3,18 @@ import SwiftUI
 struct MealView: View {
     @State var meal: Meal
 
+    @AppStorage(wrappedValue: true, Settings.showEmoji)
+    private var showEmoji: Bool
+
+    @AppStorage(wrappedValue: .Students, Settings.priceGroup)
+    private var priceGroup: PriceGroup
+
     var body: some View {
         VStack(alignment: .leading) {
-            Text("\(meal.emoji) \(meal.name)")
+            Text(showEmoji ? "\(meal.emoji) \(meal.name)" : meal.name)
                 .bold()
             
-            if let price = meal.price.getFor(group: .Students).formatPrice() {
+            if let price = meal.price.getFor(group: priceGroup).formatPrice() {
                 Text(price)
             }
         }
