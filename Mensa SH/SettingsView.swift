9125721a97732f30f92204b405d708102a388ae8
hoppjan <83405332+hoppjan@users.noreply.github.com>
2026-05-14 19:30:22 +0200

    Fix setting key accessibility & indentation

--- Mensa SH/SettingsView.swift
+++ Mensa SH/SettingsView.swift
@@ -1,64 +1,65 @@
 import SwiftUI
 
-private let settingsShowDate = "settings.showDate"
-private let settingsShowEmoji = "settings.showEmoji"
-private let settingsPriceGroup = "settings.priceGroup"
-private let settingsCity = "settings.city"
-private let settingsLocations = "settings.locations"
-private let settingsDietFilter = "settings.dietFilter"
-private let settingsAllergens = "settings.allergens"
+class Settings {
+    static let showDate = "settings.showDate"
+    static let showEmoji = "settings.showEmoji"
+    static let priceGroup = "settings.priceGroup"
+    static let city = "settings.city"
+    static let locations = "settings.locations"
+    static let dietFilter = "settings.dietFilter"
+    static let allergens = "settings.allergens"
+}
 
 struct SettingsView: View {
-    @AppStorage(wrappedValue: true, settingsShowDate)
+    @AppStorage(wrappedValue: true, Settings.showDate)
     private var showDate: Bool
     
-    @AppStorage(wrappedValue: true, settingsShowEmoji)
+    @AppStorage(wrappedValue: true, Settings.showEmoji)
     private var showEmoji: Bool
 
-    @AppStorage(wrappedValue: .Students, settingsPriceGroup)
+    @AppStorage(wrappedValue: .Students, Settings.priceGroup)
     private var priceGroup: PriceGroup
     
-    @AppStorage(wrappedValue: .Lübeck, settingsCity)
+    @AppStorage(wrappedValue: .Lübeck, Settings.city)
     private var city: City
     
-    //@AppStorage(wrappedValue: [.MensaLuebeck, .CafeteriaLuebeck, .BitsAndBytes], settingsLocations)
+    //@AppStorage(wrappedValue: [.MensaLuebeck, .CafeteriaLuebeck, .BitsAndBytes], Settings.locations)
     //private var locations: [Location]
 
-    @AppStorage(wrappedValue: .None, settingsDietFilter)
+    @AppStorage(wrappedValue: .None, Settings.dietFilter)
     private var dietFilter: DietFilter
     
-    //@AppStorage(wrappedValue: [], settingsAllergens)
+    //@AppStorage(wrappedValue: [], Settings.allergens)
     //private var settingsAllergens: [Allergens]
     
     var body: some View {
         Form {
-                Section {
-                    Toggle("Datum anzeigen", isOn: $showDate)
-                    Toggle("Emoji anzeigen", isOn: $showEmoji)
-                } header: {
-                    Text("Allgemein")
+            Section {
+                Toggle("Datum anzeigen", isOn: $showDate)
+                Toggle("Emoji anzeigen", isOn: $showEmoji)
+            } header: {
+                Text("Allgemein")
+            }
+        
+            Section {
+                Picker("Preisgruppe", selection: $priceGroup) {
+                    ForEach(PriceGroup.allCases, id: \.self) { priceGroup in
+                        Text(priceGroup.rawValue)
+                    }
                 }
-            
-                Section {
-                    Picker("Preisgruppe", selection: $priceGroup) {
-                        ForEach(PriceGroup.allCases, id: \.self) { priceGroup in
-                            Text(priceGroup.rawValue)
-                        }
+                Picker("Stadt", selection: $city) {
+                    ForEach(City.allCases, id: \.self) { city in
+                        Text(city.rawValue)
                     }
-                    Picker("Stadt", selection: $city) {
-                        ForEach(City.allCases, id: \.self) { city in
-                            Text(city.rawValue)
-                        }
-                    }
-                    Picker("Ernährungstyp", selection: $dietFilter) {
-                        ForEach(DietFilter.allCases, id: \.self) { diet in
-                            Text(diet.rawValue)
-                        }
-                    }
-                } header: {
-                    Text("Mensaoptionen")
                 }
-            
+                Picker("Ernährungstyp", selection: $dietFilter) {
+                    ForEach(DietFilter.allCases, id: \.self) { diet in
+                        Text(diet.rawValue)
+                    }
+                }
+            } header: {
+                Text("Mensaoptionen")
+            }
         }
         .navigationTitle("Einstellungen")
     }
