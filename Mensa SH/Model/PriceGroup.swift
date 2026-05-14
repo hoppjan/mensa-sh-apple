65e92ccde9bc2cfce7e7d3c38fd3bb42ab2f9d87
hoppjan <83405332+hoppjan@users.noreply.github.com>
2026-05-14 19:22:24 +0200

    Add protocols for later

--- Mensa SH/Model/PriceGroup.swift
+++ Mensa SH/Model/PriceGroup.swift
@@ -1,5 +1,11 @@
-enum PriceGroup: Codable {
-    case Students
-    case Employees
-    case Guests
+enum PriceGroup: String, Codable, CaseIterable {
+    case Students = "Students"
+    case Employees = "Employees"
+    case Guests = "Guests"
+}
+
+extension PriceGroup: Identifiable {
+    var id: String {
+        self.rawValue
+    }
 }
