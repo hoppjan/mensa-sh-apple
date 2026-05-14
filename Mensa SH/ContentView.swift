ec83d751e3fae28813b8623bd97a56e74a3910c9
hoppjan <83405332+hoppjan@users.noreply.github.com>
2026-05-14 18:06:53 +0200

    Move MealList out of ContentView

--- Mensa SH/ContentView.swift
+++ Mensa SH/ContentView.swift
@@ -3,49 +3,11 @@ import SwiftData
 
 struct ContentView: View {
     @State private var title = "Mensa SH"
-    @State private var mensaDay: MensaDay? = nil
-    @State private var meals: [Meal] = [
-        Meal(
-            name: "Lecker Essen",
-            date: "2026-05-15",
-            price: GroupedPrices(students: 2, employees: 0, guests: 0),
-            vegan: true,
-            vegetarian: true,
-            location: MensaLocation(code: "HL_ME", name: "", city: ""),
-            allergens: [],
-            language: Language.German,
-            
-        )
-    ]
 
     var body: some View {
         NavigationView {
-            List(meals) { meal in
-                Text(meal.name)
-                    .bold()
-
-                if let price = meal.price.getFor(group: .Students).formatPrice() {
-                    Text(price)
-                }
-            }
-            .task {
-                var dateComponent = DateComponents()
-                dateComponent.day = 1
-                requestMensaDays(
-                    locations: [
-                        MensaLocation(code: Location.BitsAndBytes.rawValue, name: "", city: ""),
-                        MensaLocation(code: Location.MensaLuebeck.rawValue, name: "", city: ""),
-                    ],
-                    date: Calendar.current.date(byAdding: dateComponent, to: Date.now)!,
-                    lang: Language.German,
-                    completion: { apiResponse in
-                        guard let day = apiResponse.toMensaDays().first else { return }
-                        self.mensaDay = day
-                        self.meals = day.meals
-                    }
-                )
-            }
-            .navigationTitle(title)
+            MealList()
+                .navigationTitle(title)
         }
     }
 }
---
+++ Mensa SH/MealList.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct MealList: View {
+    @State var meals: [Meal] = []
+
+    var body: some View {
+        List(meals) { meal in
+            Text(meal.name)
+                .bold()
+            
+            if let price = meal.price.getFor(group: .Students).formatPrice() {
+                Text(price)
+            }
+        }
+        .task {
+            var dateComponent = DateComponents()
+            dateComponent.day = 1
+            requestMensaDays(
+                locations: [
+                    MensaLocation(code: Location.MensaLuebeck.rawValue, name: "Mensa", city: "Lübeck"),
+                    MensaLocation(code: Location.CafeteriaLuebeck.rawValue, name: "Cafeteria", city: "Lübeck"),
+                    MensaLocation(code: Location.BitsAndBytes.rawValue, name: "Bits + Bytes", city: "Lübeck"),
+                ],
+                date: Calendar.current.date(byAdding: dateComponent, to: Date.now)!,
+                lang: Language.German,
+                completion: { apiResponse in
+                    guard let day = apiResponse.toMensaDays().first else { return }
+                    self.meals = day.meals
+                }
+            )
+        }
+    }
+}
